daf0a07ba645566b4efbdbb3f48fcb34618c57f7
rahulsingh_toxsl <rahul.singh@toxsl.com>
2021-06-27 12:32:27 +0530

    initial commit

---
+++ app/Http/Controllers/WeatherController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Weather;
+use Illuminate\Http\Request;
+
+class WeatherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        date_default_timezone_set('Asia/Kolkata');
+        if ($request->temperature != 'nan' && $request->humidity != 'nan') {
+            $model = new Weather();
+            $model->temperature = $request->temperature;
+            $model->humidity = $request->humidity;
+            $model->created_at = date('Y-m-d h:i:s');
+            if ($model->save()) {
+                echo "Saved";
+            }else {
+                echo "Not saved";
+            }
+            return;
+        }
+//         $myfile = fopen(base_path("/report.txt"), "r") or die("Unable to open file!");
+        
+//         while(!feof($myfile)) {
+//             $line = fgets($myfile);
+//             $arr = explode(",",$line);
+            
+//             $temperature = substr($arr[0], strpos($arr[0], "Temperature:") + 12);
+//             $humidity = substr($arr[1], strpos($arr[1], "Temperature:") + 9);
+//             $time = substr($arr[2], strpos($arr[2], "Temperature:") + 5);
+            
+//             $model = new Weather();
+//             $model->temperature = $temperature;
+//             $model->humidity = $humidity;
+//             $model->created_at = $time;
+//             $model->save();
+//         }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Weather  $weather
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Weather $weather)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Weather  $weather
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Weather $weather)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Weather  $weather
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Weather $weather)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Weather  $weather
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Weather $weather)
+    {
+        //
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,98 @@
+<?php
+use App\Models\Weather;
+use Carbon\Carbon;
+?>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Weather App</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      <!-- Fonts -->
+      <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+      <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+   </head>
+   <body class="antialiased">
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Weather App</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+      <div class="container">
+         <div class="row">
+            <div class="col-md-12">
+               <div id="chart"></div>
+            </div>
+         </div>
+      </div>
+      <?php 
+      $getAll = Weather::whereDate('created_at', Carbon::today())->get();
+      $temperature = [];
+      $humidity = [];
+      $date = [];
+      
+          foreach ($getAll as $value) {
+              $temperature[] = $value->temperature;
+              $humidity[] = $value->humidity;
+              $dt = new DateTime($value->created_at);
+              $dt->setTimezone(new DateTimeZone('UTC'));
+              $date[] = $dt->format('Y-m-d\TH:i:s.u\Z');
+          }
+          
+          $temp = implode(",", $temperature);
+          $humi = implode(",", $humidity);
+          
+          $datee = json_encode($date);
+          //ddd($datee);
+          //$dat = implode(",", $datee);
+          
+      ?>
+      <script>
+        var options = {
+          series: [
+          {
+          name: 'Humidity',
+          data: [{{ $humi }}]
+        },{
+          name: 'Temperature',
+          data: [
+          	{{ $temp }}
+          ]
+        }],
+          chart: {
+          height: 350,
+          type: 'area'
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+        xaxis: {
+          type: 'datetime',
+          categories: <?php echo $datee; ?>
+        },
+        tooltip: {
+          x: {
+            format: 'dd/MM/yy HH:mm'
+          },
+        },
+        };
+
+        var chart = new ApexCharts(document.querySelector("#chart"), options);
+        chart.render();
+      </script>
+   </body>
+</html>
